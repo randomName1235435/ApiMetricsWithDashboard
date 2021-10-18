c4be367d7b4aed760f013da81e0994f9aaded606
mm <unam3d@gmx.de>
2021-10-18 18:52:21 +0200

    cleanup

--- ApiMetricsWithDashboard.Api/Program.cs
+++ ApiMetricsWithDashboard.Api/Program.cs
@@ -1,13 +1,4 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting.Server.Abstractions;
-using App.Metrics.Formatters.Ascii;
 using Microsoft.Extensions.Hosting;
 using App.Metrics.AspNetCore;
 using App.Metrics.Formatters.Prometheus;
@@ -38,20 +29,5 @@ namespace ApiMetricsWithDashboard.Api
         }
 
         public static void Main(string[] args) { BuildHost(args).Run(); }
-        //MetricsAspNetTrackingWebHostBuilderExtensions.(
-
-
-
-
-        //            .UseMetrics(options =>
-        //            {
-        //                options.EndpointOptions = endpointOptions =>
-        //{
-        //endpointOptions.Me
-
-        //}
-        //            }).
-
-
     }
 }
